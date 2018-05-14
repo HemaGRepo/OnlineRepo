1da13fc19adf837b9a798a7d8e2930092db2e97e
HemaGRepo <hema.g@capgemini.com>
2018-05-14 12:44:20 +0530

    V5.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,6 +11,6 @@ public class HelloWorld
 	for(int i=0;i<10;i++)
         System.out.println( "Hello World!...."+i );
 	System.out.println( "___________________");
-	System.out.println( "Thank You");
+	System.out.println( "Jenkins CI/CD...................");
     }
 }
