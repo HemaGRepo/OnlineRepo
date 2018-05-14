f53bc439f72540f6f212472d944d7aa283493f0e
HemaGRepo <hema.g@capgemini.com>
2018-05-14 12:32:21 +0530

    V3.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@
  */
 public class HelloWorld
 {
-    //public static void main( String[] args )
+    public static void main( String[] args )
     {
 	    System.out.println( "Welcome to GITHub................");
 	    System.out.println( "___________________");
