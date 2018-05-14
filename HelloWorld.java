37e9da9e44f245b420d2d4ebf3f9a0d03d4ec3f6
HemaGRepo <hema.g@capgemini.com>
2018-05-14 12:29:06 +0530

    Add files via upload
    V1.0

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    //public static void main( String[] args )
+    {
+	for(int i=0;i<10;i++)
+        System.out.println( "Hello World!...."+i );
+	System.out.println( "___________________");
+	System.out.println( "Thank You");
+    }
+}
